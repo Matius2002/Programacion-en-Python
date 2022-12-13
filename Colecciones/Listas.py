12e9a0e26da17c2c0e4f194dab2c41d09b5b5a54
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-12-13 11:40:19 -0500

    Add files via upload

---
+++ Colecciones/Listas.py
@@ -0,0 +1,21 @@
+"""
+Listas
+Nota: Parecidas a los arreglos o vectores
+"""
+lista = [5,4,-7,9,0,1,3] #*2 Se duplica la lista
+
+# lista.sort()#Ordena la lista ascendentemente
+lista.sort(reverse=True)#Ordena la lista descendentemente
+
+# lista.pop(3)#Elimina el elemento en el indice que uno quiera
+# lista.remove(2)#Elimina el elemento que uno quiera
+# lista.clear()#Elimina todos los elementos en la lista
+# lista.reverse()#Pone al contrario los elementos
+# lista.append(6)#Agrega un elemento al final de la lista
+# lista.insert(2,3)#Se agrega un elemento en un lugar especifico
+# lista.extend([12,30,41])#Agregamos elementos al final
+
+# print("Alejandro" in lista) # Si el elemento esta en la lista
+# print(lista.index("Alejandro"))#En que indice esta el elemento
+# print(lista.count(1))#Cuenta cuantas veces aparece un elemento en la lista
+print(lista)
