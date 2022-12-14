bb14146e841a2d406f0bdc8e97a84d8810556593
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-12-13 19:30:33 -0500

    Add files via upload

---
+++ Colecciones/Tuplas.py
@@ -0,0 +1,15 @@
+"""
+Tuplas
+Nota: Tipo de dato primitivo. Son contenedores para el almacenamiento de una coleccion
+ordenada de uno o mas elementos. Los valores que tiene al incio son los que tiene al final.
+-No se puede agregar elementos mediante metodos.
+-No se puede modificar sus elementos mediante metodos.
+-No se puede eliminar sus elementos mediante metodos.
+-Se puede buscar sus elementos mediante metodos.
+-Se pueden convertir tuplas en listas y listas en tuplas.
+"""
+print()
+tupla = (4,"Hola",3.14,[1,2,3],4)#Una tupla puede alamacernar diferentes colecciones
+lista = list(tupla)#Clase list para convertir la tupla en lista
+
+print(lista)#Nos muestra la lista
